0985fa8bc8cb474f3e83795eb7380fa1943a55a6
rifaldikn <rifaldikn@gmail.com>
2023-06-18 12:28:30 +0700

    add carousel image & custom style bullet navigation

--- src/views/register/CarouselSection.vue
+++ src/views/register/CarouselSection.vue
@@ -1,19 +1,4 @@
-<template>
-  <swiper
-    :pagination="{
-      dynamicBullets: true,
-    }"
-    :autoplay="{
-      delay: 2500,
-      disableOnInteraction: false,
-    }"
-    :modules="modules"
-  >
-    <swiper-slide>Slide 1</swiper-slide>
-    <swiper-slide>Slide 2</swiper-slide><swiper-slide>Slide 3</swiper-slide>
-  </swiper>
-</template>
-<script>
+<script setup>
 // Import Swiper Vue.js components
 import { Swiper, SwiperSlide } from "swiper/vue";
 
@@ -25,15 +10,90 @@ import "swiper/css/pagination";
 // import required modules
 import { Autoplay, Pagination } from "swiper";
 
-export default {
-  components: {
-    Swiper,
-    SwiperSlide,
+import Ilustration_1 from "@/assets/images/carousel-register/Illustration-1.svg";
+import Ilustration_2 from "@/assets/images/carousel-register/Illustration-2.svg";
+import Ilustration_3 from "@/assets/images/carousel-register/Illustration-3.svg";
+import Masking from "@/assets/images/carousel-register/Masking-1.svg";
+
+const modules = [Pagination, Autoplay];
+
+const carouselContents = [
+  {
+    title: "A few clicks away from being a part of Axdif",
+    subtitle: "Manage Task Easily & Efficiently",
+    description:
+      "Increase employee productivity and performance in a measurable and precision system in one application.",
+    img: Ilustration_1,
   },
-  setup() {
-    return {
-      modules: [Pagination, Autoplay],
-    };
+  {
+    title: "A few clicks away from being a part of Axdif",
+    subtitle: "Communication Is Key",
+    description:
+      "Connect with your coworkers anywhere like you are in one workspace. Very simple and fast.",
+    img: Ilustration_2,
   },
-};
+  {
+    title: "A few clicks away from being a part of Axdif",
+    subtitle: "Employee Self-Service",
+    description:
+      "Simplify the administrative affairs of the company where you work wherever and whenever. Let’s get started with all these conveniences",
+    img: Ilustration_3,
+  },
+];
 </script>
+
+<template>
+  <swiper
+    :pagination="{
+      dynamicBullets: true,
+      clickable: true,
+      dynamicMainBullets: 3,
+    }"
+    :autoplay="{
+      delay: 3000,
+      disableOnInteraction: false,
+    }"
+    :modules="modules"
+  >
+    <swiper-slide v-for="item in carouselContents" :key="item.subtitle">
+      <v-img :src="Masking" height="100vh" cover>
+        <v-row class="fill-height justify-center text-center pa-10">
+          <v-col cols="12">
+            <div class="text-h4 font-weight-bold" style="margin-top: 10%">
+              {{ item.title }}
+            </div>
+          </v-col>
+
+          <v-col cols="12">
+            <v-img :src="item.img" height="300"></v-img>
+          </v-col>
+
+          <v-col>
+            <div class="text-h5 font-weight-bold mb-5">{{ item.subtitle }}</div>
+            <div class="text-subtitle-1">{{ item.description }}</div>
+          </v-col>
+        </v-row>
+      </v-img>
+    </swiper-slide>
+  </swiper>
+</template>
+
+<style>
+.swiper-pagination-bullet {
+  width: 16px;
+  height: 16px;
+  /* UI Properties */
+  background: #ffffff 0% 0% no-repeat padding-box;
+  border-radius: 12px;
+  opacity: 1;
+}
+
+.swiper-pagination-bullet-active {
+  color: #fff;
+  background: #08539a;
+  width: 50px;
+  height: 16px;
+  border-radius: 12px;
+  opacity: 1;
+}
+</style>
--- src/views/register/RegisterPage.vue
+++ src/views/register/RegisterPage.vue
@@ -11,7 +11,7 @@ const successRegister = ref(false);
   <v-container class="fill-height pa-0" fluid>
     <v-row class="fill-height" no-gutters>
       <v-col cols="3" class="bg-primary">
-        <v-card max-width="640px" height="100%">
+        <v-card max-width="640px" height="100%" color="blue">
           <Carousel style="height: 100%" />
         </v-card>
       </v-col>
